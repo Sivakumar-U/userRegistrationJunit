3da0f8a9cfd04bba805a8e998be0b16beb173ba2
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-24 19:58:11 +0530

    Added: Test and validation file for password contains 8 characters with atleast 1 upper case

--- src/main/java/com/userregistrationtest/UserRegistration.java
+++ src/main/java/com/userregistrationtest/UserRegistration.java
@@ -3,7 +3,7 @@ package com.userregistrationtest;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	private static final String PASSWORD_PATTERN = "^[\\w]{8,}$";
+	private static final String PASSWORD_PATTERN = "^(?=.*?[A-Z]).{8,}";
 
 	public boolean validatePassword(String password) {
 		return Pattern.matches(PASSWORD_PATTERN, password);
--- src/test/java/com/userregistrationtest/UserRegistrationTest.java
+++ src/test/java/com/userregistrationtest/UserRegistrationTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 public class UserRegistrationTest {
 
 	@Test
-	public void testPassword8Chars_WhenProper_ShouldReturnTrue() {
+	public void testPassword8Chars_Should1UpperCase_WhenProper_ShouldReturnTrue() {
 		UserRegistration validator = new UserRegistration();
-		boolean result = validator.validatePassword("sivakuma1");
+		boolean result = validator.validatePassword("sivaKuma1");
 		Assert.assertEquals(true, result);
 	}
 }
